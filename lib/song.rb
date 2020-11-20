2f15746087fd258d52eb100fa792586c11f78554
nmhummel <natalie.hummel@gmail.com>
2020-11-20 13:58:42 -0600

    commit tree c41cd58a1dc08d2e661faba8da332804c4b99391

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,14 @@
 require_relative "./interactive_record.rb"
 
+# Notice that the Song class inherits from InteractiveRecord. This means that all of the methods defined in InteractiveRecord are available to Song.
+
 class Song < InteractiveRecord
 
-  self.column_names.each do |col_name|
+  self.column_names.each do |col_name|  # self.column_names method inherited from Superclass
     attr_accessor col_name.to_sym
   end
 
 end
+
+
+# The only code the Song class needs to contain is the code to create the attr_accessors specific to itself. But even that code uses a method, #column_names, inherited from the super class.
